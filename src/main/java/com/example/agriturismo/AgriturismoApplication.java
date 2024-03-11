4e7847d07576df14ce7db4b80d6bc895aafe86fd
FedericoMarrone95 <federico.marrone95@gmail.com>
2024-03-11 15:30:42 +0100

    agriturismo

---
+++ src/main/java/com/example/agriturismo/AgriturismoApplication.java
@@ -0,0 +1,13 @@
+package com.example.agriturismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriturismoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriturismoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/agriturismo/AgriturismoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.agriturismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriturismoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
