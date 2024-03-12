cc532ff267b40e18e52bc6a8355ceb2170de70ef
FedericoMarrone95 <federico.marrone95@gmail.com>
2024-03-12 18:15:04 +0100

    Update ProdottoServiceImpl.java

--- src/main/java/com/example/agriturismo/service/ProdottoServiceImpl.java
+++ src/main/java/com/example/agriturismo/service/ProdottoServiceImpl.java
@@ -5,15 +5,18 @@ import com.example.agriturismo.model.Prodotto;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.regex.Pattern;
 
 
 @Service
 public class ProdottoServiceImpl implements ProdottoService {
     @Autowired
     private ProdottoDao prodottoDao;
+    @Autowired
+    private TipologiaService tipologiaService;
     @Override
     public List<Prodotto>  getProdotti() {
         List<Prodotto> prodotti= (List<Prodotto>) prodottoDao.findAll();
@@ -29,8 +32,41 @@ public class ProdottoServiceImpl implements ProdottoService {
     }
 
     @Override
-    public Object validaProdotto(Prodotto prodotto, String nome, String descrizione, String prezzo, String scorte, int id_tipologia){
-
+    public void registraProdotto(Prodotto prodotto, String nome, String descrizione, String prezzo, String scorte, int idTipologia, MultipartFile immagine){
+        prodotto.setNome(nome);
+        prodotto.setDescrizione(descrizione);
+        prodotto.setPrezzo(Double.parseDouble(prezzo));
+        prodotto.setScorte(Integer.parseInt(scorte));
+        prodotto.setTipologia(tipologiaService.getTipologiaById(idTipologia));
+        if(immagine != null && !immagine.isEmpty()){
+            try{
+                String formato = immagine.getContentType();
+                String immagineCodificata = "data:" + formato + ";base64," +
+                        Base64.getEncoder().encodeToString(immagine.getBytes());
+                prodotto.setImmagine(immagineCodificata);
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+    @Override
+    public Object validaProdotto(Prodotto prodotto, String nome, String descrizione, String prezzo, String scorte, int idTipologia){
+        prodotto.setTipologia(tipologiaService.getTipologiaById(idTipologia));
+        prodotto.setNome(nome);
+        Map<String, String> errori = new HashMap<>();
+        if(!Pattern.matches("[a-zA-Z0-9\\sàèìòù,.-]{1,50}", nome)){
+            errori.put("nome", "Caratteri non ammessi");
+        }
+        try{
+            prodotto.setPrezzo(Double.parseDouble(prezzo));
+        }catch (Exception e){
+            errori.put("prezzo", "il prezzo indicato non è corretto");
+        }
+        if(errori.size()>0){
+            return new Object[]{prodotto, errori};
+        } else{
+            return null;
+        }
     }
 
 }
