b98be265a3925dbaedc294d9978bf1979ff96c56
Marco Farella <marcoalfarella@gmail.com>
2024-03-13 12:24:18 +0100

    Implementazione DettaglioController

---
+++ src/main/java/com/example/agriturismo/controller/DettaglioController.java
@@ -0,0 +1,43 @@
+package com.example.agriturismo.controller;
+import com.example.agriturismo.model.Prodotto;
+import com.example.agriturismo.service.ProdottoService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+// localhost:8080/dettaglio?id=...
+@Controller
+@RequestMapping("/dettaglio")
+public class DettaglioController
+{
+  @Autowired
+  private ProdottoService prodottoService;
+
+  @GetMapping
+  public String getPage(
+          @RequestParam("id") int id,
+          Model model,
+          @RequestParam(name = "add", required = false) String result
+  )
+  {
+    Prodotto prodotto = prodottoService.getProdottoById(id);
+    model.addAttribute("prodotto", prodotto);
+    model.addAttribute("result", result);
+    return "dettaglio";
+  }
+
+  @GetMapping("/aggiungi")
+  public String add(
+          @RequestParam("id") int id,
+          HttpSession session
+  )
+  {
+    if(!prodottoService.aggiungiACarrello(id,session))
+      return "redirect:/dettaglio?id=" + id + "&add=n";
+    return "redirect:/dettaglio?id=" + id + "&add=y";
+  }
+}
--- src/main/java/com/example/agriturismo/service/ProdottoServiceImpl.java
+++ src/main/java/com/example/agriturismo/service/ProdottoServiceImpl.java
@@ -3,12 +3,14 @@ package com.example.agriturismo.service;
 import com.example.agriturismo.dao.ProdottoDao;
 import com.example.agriturismo.model.Prodotto;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.*;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -29,6 +31,86 @@ public class ProdottoServiceImpl implements ProdottoService {
         if(prodottoOptional.isPresent())
             return prodottoOptional.get();
         return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean aggiungiACarrello(int id, HttpSession session)
+    {
+        // recuperiamo il prodotto che si vuole aggiungere
+        Prodotto prodotto = getProdottoById(id);
+        // capiamo se l'utente ha già un carrello oppure no
+        List<Prodotto> carrello;
+        if(session.getAttribute("carrello") != null) // lo ha già
+        {
+            // recuperiamo il carrello salvato in sessione
+            carrello = (List<Prodotto>) session.getAttribute("carrello");
+            // capiamo se il prodotto da acquistare è già nel carrello
+            for(Prodotto p : carrello)
+                if(p.getId() == id)
+                    return false;
+            // se il prodotto non è già presente, lo aggiungiamo
+            carrello.add(prodotto);
+            // sovrascriviamo il valore del carrello in sessione
+            session.setAttribute("carrello", carrello);
+            return true;
+        }
+        else // non ha nulla (primo acquisto)
+        {
+            // creiamo un nuovo carrello e ci aggiungiamo il prodotto
+            carrello = new ArrayList<>();
+            carrello.add(prodotto);
+            // salviamo il carrello come attributo di sessione
+            session.setAttribute("carrello", carrello);
+            return true;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<Prodotto> getCarrello(HttpSession session)
+    {
+        if(session.getAttribute("carrello") != null)
+            return (List<Prodotto>) session.getAttribute("carrello");
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void rimuoviDalCarrello(int id, HttpSession session)
+    {
+        // otteniamo il carrello dalla sessione (sicuri di averlo)
+        List<Prodotto> carrello = (List<Prodotto>) session.getAttribute("carrello");
+        // rimuoviamo il prodotto dal carrello (versione base)
+        for(Prodotto p : carrello)
+            if(p.getId() == id)
+            {
+                carrello.remove(p);
+                break;
+            }
+        // rimuoviamo il prodotto dal carrello (versione avanzata)
+        carrello = carrello
+                .stream()
+                .filter(p -> p.getId() != id)
+                .collect(Collectors.toList());
+        // capiamo se il carrello ha ancora qualcosa oppure no
+        if(carrello.size() > 0) // sovrascriviamo
+            session.setAttribute("carrello", carrello);
+        else // rimuoviamo completamente
+            session.removeAttribute("carrello");
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public double getTotaleCarrello(HttpSession session)
+    {
+        List<Prodotto> carrello = (List<Prodotto>) session.getAttribute("carrello");
+        if(carrello != null)
+            return carrello
+                    .stream()
+                    .mapToDouble(Prodotto::getPrezzo)
+                    .reduce(0.0, (p1,p2) -> p1 + p2);
+        return 0;
     }
 
 
