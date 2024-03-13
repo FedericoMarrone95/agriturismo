46a55ddb1f5b8505bf212c350bbd5b1ee9c45d4c
FedericoMarrone95 <federico.marrone95@gmail.com>
2024-03-13 09:28:32 +0100

    Implementato registraProdotto

--- src/main/java/com/example/agriturismo/service/ProdottoService.java
+++ src/main/java/com/example/agriturismo/service/ProdottoService.java
@@ -1,6 +1,7 @@
 package com.example.agriturismo.service;
 
 import com.example.agriturismo.model.Prodotto;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -10,4 +11,5 @@ public interface ProdottoService {
     Prodotto getProdottoById(int id);
 
     Object validaProdotto(Prodotto prodotto, String nome, String descrizione, String prezzo, String scorte, int id_tipologia);
+    void registraProdotto(Prodotto prodotto, String nome, String descrizione, String prezzo, String scorte, int idTipologia, MultipartFile immagine);
 }
--- src/main/java/com/example/agriturismo/service/ProdottoServiceImpl.java
+++ src/main/java/com/example/agriturismo/service/ProdottoServiceImpl.java
@@ -31,24 +31,7 @@ public class ProdottoServiceImpl implements ProdottoService {
         return null;
     }
 
-    @Override
-    public void registraProdotto(Prodotto prodotto, String nome, String descrizione, String prezzo, String scorte, int idTipologia, MultipartFile immagine){
-        prodotto.setNome(nome);
-        prodotto.setDescrizione(descrizione);
-        prodotto.setPrezzo(Double.parseDouble(prezzo));
-        prodotto.setScorte(Integer.parseInt(scorte));
-        prodotto.setTipologia(tipologiaService.getTipologiaById(idTipologia));
-        if(immagine != null && !immagine.isEmpty()){
-            try{
-                String formato = immagine.getContentType();
-                String immagineCodificata = "data:" + formato + ";base64," +
-                        Base64.getEncoder().encodeToString(immagine.getBytes());
-                prodotto.setImmagine(immagineCodificata);
-            } catch (Exception e){
-                System.out.println(e.getMessage());
-            }
-        }
-    }
+
     @Override
     public Object validaProdotto(Prodotto prodotto, String nome, String descrizione, String prezzo, String scorte, int idTipologia){
         prodotto.setTipologia(tipologiaService.getTipologiaById(idTipologia));
@@ -67,6 +50,26 @@ public class ProdottoServiceImpl implements ProdottoService {
         } else{
             return null;
         }
+    }
+    @Override
+    public void registraProdotto(Prodotto prodotto, String nome, String descrizione,
+                                 String prezzo, String scorte, int idTipologia, MultipartFile immagine){
+        prodotto.setNome(nome);
+        prodotto.setDescrizione(descrizione);
+        prodotto.setPrezzo(Double.parseDouble(prezzo));
+        prodotto.setScorte(Integer.parseInt(scorte));
+        prodotto.setTipologia(tipologiaService.getTipologiaById(idTipologia));
+        if(immagine != null && !immagine.isEmpty()){
+            try{
+                String formato = immagine.getContentType();
+                String immagineCodificata = "data:" + formato + ";base64," +
+                        Base64.getEncoder().encodeToString(immagine.getBytes());
+                prodotto.setImmagine(immagineCodificata);
+            } catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+        prodottoDao.save(prodotto);
     }
 
 }
