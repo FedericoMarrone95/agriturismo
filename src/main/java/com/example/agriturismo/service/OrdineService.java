eff2ac69d60e5feb32b862ade6bc515824294396
FedericoMarrone95 <federico.marrone95@gmail.com>
2024-03-13 15:12:47 +0100

    Update OrdineService.java

--- src/main/java/com/example/agriturismo/service/OrdineService.java
+++ src/main/java/com/example/agriturismo/service/OrdineService.java
@@ -1,7 +1,11 @@
 package com.example.agriturismo.service;
 
+import com.example.agriturismo.model.Ordine;
 import jakarta.servlet.http.HttpSession;
+
+import java.util.List;
 
 public interface OrdineService {
     void inviaOrdine(HttpSession session);
+    List<Ordine> getOrdini();
 }
