2a0cac1dbf6a4c5b6d3a3c2a63f3c8af8ec606c1
angelowb <albanoangelowb@gmail.com>
2024-03-15 10:05:39 +0100

    Modifica nav login + logo + cart

--- src/main/java/com/example/agriturismo/controller/IndexController.java
+++ src/main/java/com/example/agriturismo/controller/IndexController.java
@@ -1,8 +1,11 @@
 package com.example.agriturismo.controller;
 
+import com.example.agriturismo.model.Utente;
 import com.example.agriturismo.service.ProdottoService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,7 +14,9 @@ import java.util.List;
 @RequestMapping("/")
 public class IndexController {
     @GetMapping
-    public String getPage(){
+    public String getPage(HttpSession session, Model model){
+        Utente utente = (Utente) session.getAttribute("utente");
+        model.addAttribute("utente", utente);
         return "index";
     }
 }
